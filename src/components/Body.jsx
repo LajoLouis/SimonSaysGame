902345e4ff6f40bd300b6d53c090d17e97fa9601
lajo <ezeokefranklin@gmail.com>
2024-12-11 08:25:43 +0100

    github pages

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,34 +1,32 @@
-import { RxSwitch } from "react-icons/rx";
 import { useEffect, useState } from "react";
 
 function Body() {
   const [gameOn, setGameOn] = useState(false);
   const [sequence, setSequence] = useState([]);
   const [userInput, setUserInput] = useState([]);
-  const [isDisabled, setIsDisabled] = useState(false)
-  const [score, setScore] = useState(0) 
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [score, setScore] = useState(0);
+
+  let forestgreen = "forestgreen";
+  let darkred = "darkred";
 
   // Function to start the game
   const gameStart = () => {
     setGameOn(!gameOn);
+    setUserInput([]);
 
     // Display "Simon says"
-    const simon = document.querySelector(".simon");
-    const originalStyle = getComputedStyle(simon).visibility;
+    // code not useful anymore but kept for reference
+    // const simon = document.querySelector(".simon");
+    // const originalStyle = getComputedStyle(simon).visibility;
 
-    setTimeout(() => {
-      simon.style.visibility = "visible";
-      setTimeout(() => {
-        simon.style.visibility = originalStyle;
-      }, 3000);
-    }, 500);
+    // setTimeout(() => {
+    //   simon.style.visibility = "visible";
+    //   setTimeout(() => {
+    //     simon.style.visibility = originalStyle;
+    //   }, 3000);
+    // }, 500);
   };
-  
-
-  
-  
-  
-  
 
   // Function to shine colors
   const shine = (color, delay) => {
@@ -98,10 +96,12 @@ function Body() {
       console.log("Expected sequence:", sequence);
       if (checkArray(sequence, userInput)) {
         console.log("User input matches the sequence!");
-        setScore(score+1)
+        setScore(score + 1);
+        shine(forestgreen, 0);
       } else {
         console.log("User input does not match the sequence.");
-        setScore(0)
+        setScore(0);
+        shine(darkred, 0);
       }
       setUserInput([]); // Reset user input after comparison
     }
@@ -112,33 +112,66 @@ function Body() {
   }, [gameOn]);
 
   return (
-    <div>
-      <div className="w-[50%] mx-auto flex justify-center flex-col my-12">
-        <button onClick={gameStart}className="text-[24px] flex mx-auto my-4 outline-0 transform hover:scale-105 shadow-md transition ease duration-500 border-y-black rounded-[10px] shadow-slate-900">
-        <div className="m-auto">
-        <div className="border-[2px] border-black w-[40px] h-[20px] rounded-[100px] relative bg-[#f5f5f5]">
-            <div id="switches" className={!gameOn ? "right": "left"}></div>
+    <div className="bg-[url('/img/background1.jpeg')] bg-contain bg-repeat h-screen bg-left-top">
+      <div className="w-[50%] mx-auto flex justify-center flex-col">
+        <button
+          onClick={gameStart}
+          className="text-[24px] flex mx-auto my-4 outline-0 transform hover:scale-105 shadow-md transition ease duration-500 border-y-black rounded-[10px] shadow-slate-900"
+        >
+          <div className="m-auto">
+            <div
+              className={`border-[2px] ${
+                gameOn ? "border-green-800" : "border-red-800"
+              } w-[40px] h-[20px] rounded-[100px] relative bg-[#f5f5f5]`}
+            >
+              <div id="switches" className={!gameOn ? "right" : "left"}></div>
+            </div>
           </div>
-        </div>
         </button>
-        
-        <p className="simon text-center p-[20px]" style={{visibility:"hidden"}}>{simonText}</p>
-        <div className="colorBox flex justify-center w-[300px] h-[300px] mx-auto flex-wrap rounded-full overflow-hidden">
-          <button onClick={getUserInput} disabled={isDisabled} value="red" className="red bg-red-600 w-[50%] rounded-[5px]">
+
+        <p
+          className={`simon text-center p-[20px] ${
+            gameOn ? "visible" : "hidden"
+          } text-2xl font-extrabold`}
+        >
+          Simon says Game On!!!
+        </p>
+        <div className="colorBox relative flex justify-center w-[400px] h-[400px] mx-auto flex-wrap rounded-full overflow-hidden shadow-xl shadow-black">
+          <button
+            onClick={getUserInput}
+            disabled={isDisabled}
+            value="red"
+            className={`red bg-red-600 w-[50%] shadow-md shadow-black rounded-br-[50px]`}
+          >
             Red
           </button>
-          <button onClick={getUserInput} disabled={isDisabled} value="blue" className="blue bg-blue-600 w-[50%] rounded-[5px]">
+          <button
+            onClick={getUserInput}
+            disabled={isDisabled}
+            value="blue"
+            className={`blue bg-blue-600 w-[50%] shadow-md shadow-black rounded-bl-[50px]`}
+          >
             Blue
           </button>
-          <button onClick={getUserInput} disabled={isDisabled} value="green" className="green bg-green-600 w-[50%] rounded-[5px]">
+          <div className="darkred forestgreen text-center m-7 bg-black text-white font-extrabold w-[40px] h-[40px] rounded-full flex flex-col justify-center absolute top-[150px]">
+            {score}
+          </div>
+          <button
+            onClick={getUserInput}
+            disabled={isDisabled}
+            value="green"
+            className={`green bg-green-600 w-[50%] shadow-md shadow-black rounded-tr-[50px]`}
+          >
             Green
           </button>
-          <button onClick={getUserInput} disabled={isDisabled} value="yellow" className="yellow bg-yellow-600 w-[50%] rounded-[5px]">
+          <button
+            onClick={getUserInput}
+            disabled={isDisabled}
+            value="yellow"
+            className={`yellow bg-yellow-600 w-[50%] shadow-md shadow-black rounded-tl-[50px]`}
+          >
             Yellow
           </button>
-        </div>
-        <div className="text-center m-7">
-          {score}
         </div>
       </div>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,29 @@
 import { GiGamepad } from "react-icons/gi";
+import { BiMenu } from "react-icons/bi";
+import { useState } from "react";
 
 function Header() {
+  const [showHow, setShowHow] = useState(false)
   return (
     <div>
-        <div className="flex p-[15px] justify-center text-[34px] font-extrabold text-teal-950 custom-gradient">
-        <h1>Simon Says</h1>
-        <GiGamepad className="text-5xl"/>
+        
+        <div className="text-teal-950 custom-gradient">
+          <div>
+            
+          </div>
+          <div className="flex p-[15px] relative justify-center text-[34px] font-extrabold text-black custom-gradient">
+            <BiMenu className='text-3xl text-teal-950 cursor-pointer absolute top-0 left-0 ml-7' onClick={()=>setShowHow(!showHow)}/>
+            <h1>Simon Says</h1>
+            <GiGamepad className="text-5xl"/>
+          </div>
+        </div>
+        <div className= {`fixed top-[80px] h-screen left-0 bg-white text-black transition-all duration-200 ${showHow ? "w-[200px] visible": "w-0 hidden"}`}>
+          <ol className="p-[20px] list-disc flex flex-col space-y-5">
+            <li>Click on the switch to start game</li>
+            <li>Allow the colors to shine finish</li>
+            <li>Select the colors in the exact order for you to increaase your points</li>
+            <li>Repeat the process</li>
+          </ol>
         </div>
     </div>
   )
